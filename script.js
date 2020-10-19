f08b4553b13f3cdb3dec531491e693952fb5e365
rainydaysavings <eliseuamaro@protonmail.com>
2020-10-19 17:31:34 +0100

    JS stuff happens

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function getIndex(j) {
-    var x = j.cellIndex;
-
-    alert(x);
+function genDivs(v) {
+    var e = document.body;
+    for (var i = 0; i < v; i++) {
+        var row = document.createElement("div");
+        row.className = "row";
+        for (var x = 1; x <= v; x++) {
+            var cell = document.createElement("div");
+            cell.className = "gridsquare";
+            cell.innerText = (i * v) + x;
+            row.appendChild(cell);
+        }
+        e.appendChild(row);
+    }
+    document.getElementById("code").innerText = e.innerHTML;
 }
 
-function getRow(j) {
-    var y = j.rowIndex;
+function instr() {
+    var rules = document.getElementById("no-show");
+    var rotate = document.getElementById("rotate-me");
 
-    alert(y);
+    if (rules.style.display === "none" || !rules.style.display) {
+        rules.style.display = "block";
+        rotate.style.transform = "rotate(180deg)";
+    } else {
+        rules.style.display = "none";
+        rotate.style.transform = "rotate(0deg)";
+    }
 }
