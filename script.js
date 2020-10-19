033a6d828ea4fc2948168ed453998e30c4a78486
rainydaysavings <eliseuamaro@protonmail.com>
2020-10-19 13:05:15 +0100

    JS e cenas

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-$('table td').click(function() {
-    for (var x = 0; x <= $(this).index(); x++) {
-        for (var y = 0; y <= $(this).parent().index(); y++) {
-            $(this).addClass('selected');
-        }
-    }
-}, function() {
-    $('table td').removeClass('selected');
-});
+function getIndex(j) {
+    var x = j.cellIndex;
+
+    alert(x);
+}
+
+function getRow(j) {
+    var y = j.rowIndex;
+
+    alert(y);
+}
