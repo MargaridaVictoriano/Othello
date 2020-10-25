d2dae5bbfe2823d55585fdbfdbf8846efc267cf7
rainydaysavings <eliseuamaro@protonmail.com>
2020-10-25 15:09:58 +0000

    Table generated, respective div removed once logged out

--- script.js
+++ script.js
@@ -5,22 +5,34 @@ class login {
     }
 
     start() {
-        document.getElementById("login").style.display="block";
-        document.getElementById("sign-out").style.display="none";
-        document.getElementById("uname").style.display="none";
+        // Makes account info invisible
+        document.getElementById("sign-out").style.display = "none";
+        document.getElementById("uname").style.display = "none";
+        document.getElementById("config").style.display = "none";
+        // Makes login form Visible
+        document.getElementById("login").style.display = "block";
+        // Removes the table
+        this.removeDivs('inner-table');
     }
 
     enter() {
+        // Get info from form
         this.username = document.getElementById("username").value
         this.password = document.getElementById("password").value
 
-        if ( this.username == "admin" && this.password == "password") {
-            document.getElementById("uname").innerHTML=this.username;
-            document.getElementById("uname").style.display="block";
-            document.getElementById("sign-out").style.display="block";
+        if (this.username === "admin" && this.password === "password") {
+            document.getElementById("uname").innerHTML = this.username;
+            document.getElementById("uname").style.display = "block";
+            document.getElementById("sign-out").style.display = "block";
             document.getElementById("login").style.display = "none";
             new config().start();
         }
+    }
+
+    removeDivs(elementId) {
+        // Removes an element and it's children from the document
+        const element = document.getElementById(elementId);
+        element.parentNode.removeChild(element);
     }
 }
 
@@ -36,10 +48,25 @@ class config {
     }
 
     beginNewGame() {
-        alert(this.versus + this.color + this.difficulty);
+        document.getElementById("config").style.display = "none";
+        this.genDivs();
     }
-}
 
-class main {
-    /**/
+    genDivs() {
+        const table = document.getElementById("table");
+        table.innerHTML += '<div id="game-info">';
+        table.innerHTML += '<div id="inner-table">';
+        const innerTable = document.getElementById("inner-table");
+        const gameInfo = document.getElementById("game-info");
+
+        for (let i = 0; i < 8; i++) {
+            innerTable.innerHTML += '<div class="row">';
+
+            for (let j = 0; j < 8; j++) {
+                innerTable.innerHTML += '<div class="gridsquare">' + (i * 5 + j + 1) + '</div>';
+            }
+
+            innerTable.innerHTML += '</div>';
+        }
+    }
 }
