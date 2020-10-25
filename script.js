c22f4bcbdeb2dd42b257fe87ca098f196c44ad05
rainydaysavings <eliseuamaro@protonmail.com>
2020-10-25 18:16:49 +0000

    Can display discs and stuff

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ class login {
         document.getElementById("sign-out").style.display = "none";
         document.getElementById("uname").style.display = "none";
         document.getElementById("config").style.display = "none";
+        document.getElementById("table").style.display = "none";
         // Makes login form Visible
         document.getElementById("login").style.display = "block";
         // Removes the table
@@ -20,7 +21,7 @@ class login {
         this.username = document.getElementById("username").value
         this.password = document.getElementById("password").value
 
-        if (this.username === "admin" && this.password === "password") {
+        if (this.username === "" && this.password === "") {
             document.getElementById("uname").innerHTML = this.username;
             document.getElementById("uname").style.display = "block";
             document.getElementById("sign-out").style.display = "block";
@@ -52,21 +53,44 @@ class config {
         this.genDivs();
     }
 
+    // isto deve estar noutro sitio
     genDivs() {
         const table = document.getElementById("table");
+        table.style.display = "flex";
         table.innerHTML += '<div id="game-info">';
         table.innerHTML += '<div id="inner-table">';
         const innerTable = document.getElementById("inner-table");
         const gameInfo = document.getElementById("game-info");
 
         for (let i = 0; i < 8; i++) {
-            innerTable.innerHTML += '<div class="row">';
+            innerTable.innerHTML += '<div id=row' + i + '>';
+            let curRow = document.getElementById("row" + i);
 
             for (let j = 0; j < 8; j++) {
-                innerTable.innerHTML += '<div class="gridsquare">' + (i * 5 + j + 1) + '</div>';
-            }
+                curRow.innerHTML += '<div onclick="new actorPlay(this.color).start(id)" id=square' + i + j + '>' + '</div>';
+                let curSquare = document.getElementById("square" + i + j);
 
+                if ((i === 3 && j === 4) || (i === 4 && j === 3)) {
+                    curSquare.innerHTML += '<div class="whites">';
+                } else if ((i === 3 && j === 3) || (i === 4 && j === 4)) {
+                    curSquare.innerHTML += '<div class="blacks">';
+                }
+            }
             innerTable.innerHTML += '</div>';
         }
+    }
+}
+
+class actorPlay {
+    constructor(color) {
+        this.color = color;
+    }
+
+    start(id) {
+        let posX = id.slice(6,7);
+        let posY = id.slice(7,8);
+
+        let curSquare = document.getElementById("square" + posX + posY);
+        curSquare.innerHTML += '<div class=' + this.color.valueOf() + '>';
     }
 }
