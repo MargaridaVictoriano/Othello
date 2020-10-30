faa6dab7a465913627c0eac0660d25ec4c8d8da2
rainydaysavings <eliseuamaro@protonmail.com>
2020-10-30 13:52:19 +0000

    wow it works

--- script.js
+++ script.js
@@ -33,51 +33,48 @@ class login {
     removeDivs(elementId) {
         // Removes an element and it's children from the document
         const element = document.getElementById(elementId);
-        while(element.firstChild){
-          element.removeChild(element.firstChild);
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
         }
     }
 }
 
 class config {
-
-  constructor(){
-    this.v = "computer";
-    this.c = "blacks";
-    this.d = "easy";
-  }
+    constructor() {
+        this.v = document.getElementById("versus").value;
+        this.c = document.getElementById("color").value;
+        this.d = document.getElementById("difficulty").value;
+    }
 
     start() {
         document.getElementById("config").style.display = "block";
     }
 
     beginNewGame() {
-        this.v = document.getElementById("versus").value;
-        this.c = document.getElementById("color").value;
-        this.d = document.getElementById("difficulty").value;
         document.getElementById("config").style.display = "none";
-        this.genDivs();
-        console.log(this.v + " " + " " + this.c + " " + this.d);
+        this.genDivs(this.c);
     }
 
-    // isto deve estar noutro sitio
-    genDivs() {
+    genDivs(color) {
         let table = document.getElementById("table");
         table.style.display = "flex";
 
         for (let i = 0; i < 8; i++) {
             let row = document.createElement("div");
             table.appendChild(row);
-            row.setAttribute("id","row" + i);
+            row.setAttribute("id", "row" + i);
             let curRow = document.getElementById("row" + i);
 
             for (let j = 0; j < 8; j++) {
 
                 let piece = document.createElement("div");
 
-                piece.setAttribute("onclick","new actorPlay(id" + "," + this.c + ")" + ".start()");
-                piece.setAttribute("id","square" + i + j);
+                piece.onclick = function() {
+                    new actorPlay(i, j, color).action();
+                };
+                piece.setAttribute("id", "square" + i + j);
                 curRow.appendChild(piece);
+
                 let curSquare = document.getElementById("square" + i + j);
 
                 if ((i === 3 && j === 4) || (i === 4 && j === 3)) {
@@ -88,48 +85,23 @@ class config {
                     let blackSquare = document.createElement("div");
                     curSquare.appendChild(blackSquare);
                     blackSquare.className = "blacks";
-                  }
+                }
             }
         }
     }
 }
-function getColor(selectObject) {
-  const input = document.querySelector('input.js-addcart-detail');
-  input.value = selectObject.value;
-  console.log(input);
-}
+
 class actorPlay {
-
-    constructor(id,color){
-      this.id = id;
-      this.color = color;
+    constructor(posX, posY, color) {
+        this.posX = posX;
+        this.posY = posY;
+        this.color = color;
     }
-    start() {
-        let posX = this.id.slice(6,7);
-        let posY = this.id.slice(7,8);
 
-        let curSquare = document.getElementById("square" + posX + posY);
-        let blackSquare = document.createElement("div");
-        curSquare.appendChild(blackSquare);
-        blackSquare.className = this.color;
-        alert(this.color);
+    action() {
+        let curSquare = document.getElementById("square" + this.posX + this.posY);
+        let piece = document.createElement("div");
+        curSquare.appendChild(piece);
+        piece.className = this.color;
     }
 }
-
-/*class minimax {
-  //configuracao inicial do tabuleiro
-  function initialState(list) {
-    var matrix = [];
-    for(var i=0; i<8; i++) {
-        matrix[i] = [];
-        for(var j=0; j<8; j++) {
-
-        }
-    }
-  }
-}
-*/
-  //estado do jogo
-  //function getState(list) {
-
-//}
