6b6305fe5b774ad31ec4e8e93feb2a3976dfb84f
rainydaysavings <eliseuamaro@protonmail.com>
2020-12-05 18:30:43 +0000

    Punctuation is now aware either it is online or offline.

--- js/winCounting.js
+++ js/winCounting.js
@@ -3,29 +3,55 @@ function punctuation() {
     let nWhites = 0;
     let nBlacks = 0;
 
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            if (discs[i][j] === 1) {
-                nBlacks++;
-            } else if (discs[i][j] === 2) {
-                nWhites++;
+    if (isOnline) {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (currentBoard[i][j] === "dark") {
+                    nBlacks++;
+                } else if (currentBoard[i][j] === "light") {
+                    nWhites++;
+                }
             }
         }
-    }
-    if (nWhites >= nBlacks) {
-        increment("white");
-    }
-    if (nBlacks >= nWhites) {
-        increment("black");
+        if (nWhites >= nBlacks) {
+            increment("light");
+        }
+        if (nBlacks >= nWhites) {
+            increment("dark");
+        }
+    } else if(!isOnline) {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (discs[i][j] === 1) {
+                    nBlacks++;
+                } else if (discs[i][j] === 2) {
+                    nWhites++;
+                }
+            }
+        }
+        if (nWhites >= nBlacks) {
+            increment("white");
+        }
+        if (nBlacks >= nWhites) {
+            increment("black");
+        }
     }
 }
 
 // increments points
 function increment(winner) {
-    if (winner === "white") {
-        pointsWhite += 1;
-    } else {
-        pointsBlack += 1;
+    if (isOnline) {
+        if (winner === "light") {
+            pointsWhite += 1;
+        } else if (winner === "dark") {
+            pointsBlack += 1;
+        }
+    } else if(!isOnline) {
+        if (winner === "white") {
+            pointsWhite += 1;
+        } else if (winner === "black") {
+            pointsBlack += 1;
+        }
     }
     winnerCount();
 }
